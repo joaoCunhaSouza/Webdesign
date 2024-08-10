f88fee806f86ed7b4eaeb009f268532e6c03d572
joaoCunhaSouza <joaovictorps4slim@gmail.com>
2024-08-09 21:23:36 -0300

    Remover diretório HTML-CSS-REACT que foi adicionado por engano

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+
+export function App() {
+  return (
+    <>
+
+    <div className='img'></div>
+
+      <h1>
+        Transformando <span>espaço</span>, criando <span>história</span>
+      </h1>
+
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <span>Nulla modi soluta consectetur</span> culpa dolepe illum ipsum ipsa molestias reprehenderit ad?</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quaerat repellat, voluptatum orro <span>perspicilaboriosam veritatis fugit</span> doloribus dolore ab ipsam voluptas provident corporis?</p>
+
+
+      <div className='icon'>
+
+        <div className='icon-I'>
+          <h1>Instagram</h1>
+        </div>
+        <div>
+          <h1 className='icon-F'>Fale conosco</h1>
+        </div>
+      </div>
+    </>
+  );
+}
