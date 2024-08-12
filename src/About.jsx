82733dc84103365d94fb755817e0e7fc42c9944c
JOÃO VICTOR CUNHA DE SOUZA <joao-victor_c_souza@estudante.sesisenai.org.br>
2024-08-12 20:46:46 -0300

    Upload Files

---
+++ src/About.jsx
@@ -0,0 +1,14 @@
+// src/About.jsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About page.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias porro impedit, natus perferendis quaerat ratione voluptatibus, omnis nihil praesentium doloremque consequuntur nobis excepturi illo facere fugiat, inventore ad. Quibusdam, ducimus a est sapiente alias sunt? Omnis ex natus saepe laboriosam placeat provident magni libero ut quae necessitatibus labore, neque rem reprehenderit voluptates exercitationem quibusdam. Voluptates odio ullam commodi sed dolorum quod sunt mollitia officiis quisquam quo eos repudiandae eaque, totam tempore magni quae ratione quas vitae deserunt, eveniet voluptatem, debitis atque. Perspiciatis veritatis fuga quia totam, eum quibusdam doloremque suscipit ipsam amet voluptatibus blanditiis id repellendus corrupti praesentium hic ab laborum quam atque dolorem? Distinctio eius eaque ad libero, modi corrupti sint soluta vel blanditiis et in, ducimus dolores totam numquam cupiditate expedita velit quibusdam veniam laborum atque deserunt amet? Sit nam, aut nemo accusantium hic dolorem quia. Earum sunt reprehenderit quod sit in consequuntur, distinctio inventore reiciendis ex deserunt laborum excepturi alias. Reiciendis officia ipsam nesciunt corporis, provident, deleniti, praesentium natus autem repellendus expedita nam exercitationem dolor est aspernatur eligendi dignissimos tenetur? Cumque enim harum quod exercitationem dignissimos deleniti porro iste accusantium autem, vero tenetur est! Aperiam repellat reprehenderit exercitationem et porro? Quia blanditiis earum nemo quo dicta aspernatur culpa a eveniet, aperiam saepe vitae nihil nobis repellendus. Ratione in cumque incidunt tempora accusamus esse, eius aspernatur maxime eum fugit perspiciatis quisquam recusandae sit molestias doloremque quas, numquam, obcaecati quod odio modi odit iste dolores? Officia saepe ipsam illum deserunt similique veniam. Nam laudantium recusandae, est blanditiis pariatur assumenda!</p>
+    </div>
+  );
+};
+
+export default About;
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
+// src/App.jsx
+import React from 'react';
 import './style.css';
 import images from "./img/img.jpg";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import About from './About';
 
 export function App() {
   return (
-    <>
-      <img className='img' src={images} alt="" />
+    <Router>
+      <div>
+        {}
+        <ul>
+          <li className='Home'>
+            <Link to="/">Home</Link>
+          </li>
+          <li className='About'>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
 
-      <h1>
-        Transformando <span className="bold">espaço</span>, criando <span className="bold">história</span>
-      </h1>
+        {}
+        <Routes>
+          <Route path="/" element={<div>
+            <img className='img' src={images} alt="" />
 
-      <p>Lorem ipsum dolor sit, amet <b>consectetur</b> adipisicing elit. <span className="bold">Nulla modi soluta consectetur</span> culpa dolepe illum ipsum ipsa molestias reprehenderit ad?</p>
-      <p className='pp'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quaerat repellat, <b>voluptatum</b> orro <span className="bold">perspicilaboriosam veritatis fugit</span> doloribus dolore ab ipsam voluptas provident corporis?</p>
+            <h1>
+              Transformando <span className="bold">espaço</span>, criando <span className="bold">história</span>
+            </h1>
 
-      <div className='icon'>
-        <div className='icon-I'>
-          <a href="#">Instagram</a>
-        </div>
-        <div>
-          <a href="#">Fale conosco</a>
-        </div>
+            <p>Lorem ipsum dolor sit, amet <b>consectetur</b> adipisicing elit. <span className="bold">Nulla modi soluta consectetur</span> culpa dolepe illum ipsum ipsa molestias reprehenderit ad?</p>
+            <p className='pp'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi quaerat repellat, <b>voluptatum</b> orro <span className="bold">perspicilaboriosam veritatis fugit</span> doloribus dolore ab ipsam voluptas provident corporis?</p>
+
+            <div className='icon'>
+              <div className='icon-I'>
+                <a href="#">Instagram</a>
+              </div>
+              <div>
+                <a href="#">Fale conosco</a>
+              </div>
+            </div>
+          </div>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
