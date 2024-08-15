8547688003cb80a762886e0c9b3ba6909aa64975
Your Name <you@example.com>
2024-08-15 14:11:01 -0300

    Descrição das alterações realizadas

--- src/About.jsx
+++ src/About.jsx
@@ -1,14 +1,101 @@
 // src/About.jsx
 import React from 'react';
 
+
 const About = () => {
   return (
     <div>
       <h1>About Us</h1>
       <p>This is the About page.</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias porro impedit, natus perferendis quaerat ratione voluptatibus, omnis nihil praesentium doloremque consequuntur nobis excepturi illo facere fugiat, inventore ad. Quibusdam, ducimus a est sapiente alias sunt? Omnis ex natus saepe laboriosam placeat provident magni libero ut quae necessitatibus labore, neque rem reprehenderit voluptates exercitationem quibusdam. Voluptates odio ullam commodi sed dolorum quod sunt mollitia officiis quisquam quo eos repudiandae eaque, totam tempore magni quae ratione quas vitae deserunt, eveniet voluptatem, debitis atque. Perspiciatis veritatis fuga quia totam, eum quibusdam doloremque suscipit ipsam amet voluptatibus blanditiis id repellendus corrupti praesentium hic ab laborum quam atque dolorem? Distinctio eius eaque ad libero, modi corrupti sint soluta vel blanditiis et in, ducimus dolores totam numquam cupiditate expedita velit quibusdam veniam laborum atque deserunt amet? Sit nam, aut nemo accusantium hic dolorem quia. Earum sunt reprehenderit quod sit in consequuntur, distinctio inventore reiciendis ex deserunt laborum excepturi alias. Reiciendis officia ipsam nesciunt corporis, provident, deleniti, praesentium natus autem repellendus expedita nam exercitationem dolor est aspernatur eligendi dignissimos tenetur? Cumque enim harum quod exercitationem dignissimos deleniti porro iste accusantium autem, vero tenetur est! Aperiam repellat reprehenderit exercitationem et porro? Quia blanditiis earum nemo quo dicta aspernatur culpa a eveniet, aperiam saepe vitae nihil nobis repellendus. Ratione in cumque incidunt tempora accusamus esse, eius aspernatur maxime eum fugit perspiciatis quisquam recusandae sit molestias doloremque quas, numquam, obcaecati quod odio modi odit iste dolores? Officia saepe ipsam illum deserunt similique veniam. Nam laudantium recusandae, est blanditiis pariatur assumenda!</p>
+
+     
+
+
+
+     <form action="#" method="post">
+  <fieldset>
+    <legend>Login</legend>
+    
+    <label for="username">User</label>
+    <input type="text" name="username" id="username" placeholder="User" required />
+    
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password" placeholder="Password" required />
+    
+    <input type="submit" value="Login"/>
+  </fieldset>
+</form>
+
+<details>
+      <summary> Now more </summary>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate architecto modi, provident fugiat id sunt eius iure excepturi dicta laborum atque? Omnis eos tempora mollitia delectus ducimus provident ab quas.</p>
+     </details>
+
+
     </div>
   );
 };
 
 export default About;
+
+<table>
+      <caption>table price budget</caption>
+
+      <thead>
+        <th>Area</th>
+        <th>Cursos</th>
+        <th>Investimento</th>
+      </thead>
+
+      <tbody>
+
+        <tr>
+          <td rowSpan={3}>Design</td>
+          <td>InDesign</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+    
+          <td>Photoshop</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+      
+          <td>Illustrator</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+          <td rowSpan={4}>Development</td>
+          <td>HTML</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+          
+          <td>CSS</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+    
+          <td>Javascript</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+        <tr>
+     
+          <td>PHP</td>
+          <td>R$ 1.000.00</td>
+        </tr>
+
+      </tbody>
+
+      <tfoot>
+
+        <tr>
+          <td colSpan={3}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel, adipisci harum! Non eveniet perferendis aliquid a error illo accusantium rem doloribus asperiores, suscipit saepe autem!</td>
+        </tr>
+        
+      </tfoot>
+
+
+
+     </table>
